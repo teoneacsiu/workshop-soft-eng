61ca2f4f4fefb1b91da8edccad0b9cfacbdb8a29
Victor-Teodor Neacsiu <teobest2004@gmail.com>
2025-04-06 12:07:48 +0300

    I changed the structure of the code so that it works for the tasks

--- test/jest/gilded-rose.spec.ts
+++ test/jest/gilded-rose.spec.ts
@@ -9,7 +9,7 @@ describe('Gilded Rose', () => {
         const items = gildedRose.updateQuality();
 
         // Assert
-        expect(items[0].name).toBe('bar');
+        expect(items[0].name).toBe('foo');
     });
 
     it('sword quality drops by 1', () => {
@@ -20,6 +20,28 @@ describe('Gilded Rose', () => {
       const items = gildedRose.updateQuality();
 
       // Assert
-      expect(items[0].quality).toBe(1);
+      expect(items[0].quality).toBe(0);
+    })
+
+    it('Brie goes not over 50', () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Aged Brie', 22, 50)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBe(50);
+    })
+
+    it("sword quality doesn't drop below 0" , () => {
+      // Arrange
+      const gildedRose = new GildedRose([new Item('Sword', -1, 0)]);
+
+      // Act
+      const items = gildedRose.updateQuality();
+
+      // Assert
+      expect(items[0].quality).toBe(0);
     })
 });
